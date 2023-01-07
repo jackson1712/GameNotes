451bbdd6a01783091e44fab1fb5895ed06216b29
Jackson <jackson1712@hotmail.com>
2023-01-07 18:25:48 -0300

    added controllers for manipulate the creation and updated from users

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,17 @@
+const AppError = require("../utils/AppError");
+
+class UsersController {
+    create(request, response) {
+        const { name, email, password } = request.body;
+
+        if(!name) {
+            throw new AppError("Nome obrigatório");
+        }
+
+        response.status(201).json({ name, email, password });
+    }
+}
+
+module.exports = UsersController;
+
+// Definição do controlador de usuário, criação(POST) e atualização(PUT)
